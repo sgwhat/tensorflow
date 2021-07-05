7818c69563e5ea3b0e23a4f75dfb1c8aa1aea1aa
Wang,Quintin <quintin.wang@intel.com>
2021-07-05 14:11:32 +0800

    add DEVICE_DEFAULT for batch_kernel/concat/fact/partioned_function ops

--- tensorflow/core/kernels/partitioned_function_ops.cc
+++ tensorflow/core/kernels/partitioned_function_ops.cc
@@ -283,11 +283,14 @@ REGISTER_KERNEL_BUILDER(Name("StatefulPartitionedCall").Device(DEVICE_CPU),
                         PartitionedCallOp);
 REGISTER_KERNEL_BUILDER(Name("PartitionedCall").Device(DEVICE_GPU),
                         PartitionedCallOp);
+REGISTER_KERNEL_BUILDER(Name("PartitionedCall").Device(DEVICE_DEFAULT),
+                        PartitionedCallOp);
 REGISTER_KERNEL_BUILDER(Name("StatefulPartitionedCall").Device(DEVICE_GPU),
+                        PartitionedCallOp);
+REGISTER_KERNEL_BUILDER(Name("StatefulPartitionedCall").Device(DEVICE_DEFAULT),
                         PartitionedCallOp);
 
 REGISTER_INPUT_COLOCATION_EXEMPTION("PartitionedCall");
 REGISTER_INPUT_COLOCATION_EXEMPTION("StatefulPartitionedCall");
-
 
 }  // namespace tensorflow
